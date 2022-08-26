90a1608a3ce1720f9b4bd6e14c9370235ef39900
Ash Simmonds <ashsimmonds@gmail.com>
2022-08-27 03:19:40 +0930

    Revert "hmm vercel buggered"
    This reverts commit 1478d911533877ca5138dfbd79afabe5a9c1c678.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-// import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless';
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [alpinejs()],
-  // output: 'server',
-  // adapter: vercel(),
+  output: 'server',
+  adapter: vercel(),
 });
