c61f44713431588babd752d92b5039702f1786e7
Ash Simmonds <ashsimmonds@gmail.com>
2022-08-27 03:28:04 +0930

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import alpinejs from "@astrojs/alpinejs";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs()],
+  integrations: [alpinejs(), tailwind()],
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel()
 });
