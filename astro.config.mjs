cbdcf70a1a7e3b7c8e432acd822a0ae781aed202
Ash Simmonds <ashsimmonds@gmail.com>
2022-08-27 02:42:32 +0930

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel/serverless';
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs()]
+  integrations: [alpinejs()],
+  output: 'server',
+  adapter: vercel(),
 });
