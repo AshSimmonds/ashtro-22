179c5cc307682b35b01e9c1fe5580486a18f4c87
Ash Simmonds <ashsimmonds@gmail.com>
2022-08-29 00:05:10 +0930

    list minimal results

--- src/components/TallySoSubmissions.tsx
+++ src/components/TallySoSubmissions.tsx
@@ -3,20 +3,34 @@ import type { FunctionalComponent } from 'preact'
 
 
 const data = await fetch(
-  "https://api.airtable.com/v0/app0yd2hvlSxsieUo/submissions",
-  { headers: { Authorization: `Bearer ${import.meta.env.AIRTABLE_API_KEY}` } }
+    "https://api.airtable.com/v0/app0yd2hvlSxsieUo/submissions",
+    { headers: { Authorization: `Bearer ${import.meta.env.AIRTABLE_API_KEY}` } }
 ).then((response) =>
-  response.json()
+    response.json()
 );
 
 // Components that are build-time rendered also log to the CLI.
 // When rendered with a client:* directive, they also log to the browser console.
 console.log(data.records);
 
+
+const listOfSubmissions = data.records.map((record: {
+    fields: any; id: string; 
+}) => {
+    return (
+        <>
+            <div>
+                {record.id} | {JSON.stringify(record.fields.Address)}
+            </div>  
+        </>
+    );
+});
+
 const TallySoSubmissions: FunctionalComponent = () => {
-  return <>
-    {JSON.stringify(data.records)}
-  </>;
+    return <>
+        {listOfSubmissions}
+    </>;
+
 };
 
 export default TallySoSubmissions;
