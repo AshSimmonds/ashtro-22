d9d6cb1a50fa73a2fc07411e40f70078c73b6234
Ash Simmonds <ashsimmonds@gmail.com>
2022-09-03 20:12:47 +0930

    getting those sveltey goodness tabs working

---
+++ src/components/TabsSvelte.svelte
@@ -0,0 +1,30 @@
+<script>
+    let tabs = ["Tab 1", "Tab 2", "Tab 3"]
+
+    let activeTabIndex = 0
+</script>
+
+<div class="tabs tab-wrapper w-80 mx-auto">
+    {#each tabs as tab, index}
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <label
+            class="tab-control tab tab-lifted"
+            class:tab-active={activeTabIndex === index}
+            on:click={() => (activeTabIndex = index)}
+        >
+            {tab}
+        </label>
+    {/each}
+</div>
+
+{#if activeTabIndex == 0}
+    <div>Looks like this sort of works decent</div>
+{/if}
+
+{#if activeTabIndex == 1}
+    <div>dunno if you can link directly to the content of a tab though</div>
+{/if}
+
+{#if activeTabIndex == 2}
+    <div>content 3</div>
+{/if}
