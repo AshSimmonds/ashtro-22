ccb62aeadb1469f8b8362016ee6c61f94454be2b
Ash Simmonds <ashsimmonds@gmail.com>
2022-10-20 18:32:35 +1030

    add Vue to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import vercel from '@astrojs/vercel/serverless';
 import alpinejs from "@astrojs/alpinejs";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs(), preact(), tailwind(), svelte()],
+  integrations: [alpinejs(), preact(), tailwind(), svelte(), vue()],
   output: 'server',
   adapter: vercel()
 });
