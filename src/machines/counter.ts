e241fb158d3cc98a7d29d8f1e045f5e1a6d31f82
Ash Simmonds <ashsimmonds@gmail.com>
2022-10-20 17:34:20 +1030

    deus export machina

--- src/machines/counter.ts
+++ src/machines/counter.ts
@@ -1,7 +1,7 @@
 import { createMachine, interpret } from 'xstate';
 
 
-const counterMachine =
+export const counterMachine =
     /** @xstate-layout N4IgpgJg5mDOIC5QGMD2BXAdgFzAJwDoBDZbASwDcwBiMzZPMAWzB0VAAdVYzzVN2IAB6IAtAEYAzAFYCADgDskgCwK5k8QE4lAJgAMygDQgAnmJ3idBHXM0A2PXL3SFey3bsBfT8bRZchCTkVNQQYAzMrNiCXDx8AkjCYsqakgR2cpaZKnr2MtLGZgiiFlY29nqSCvaZ1eLevhg4+MSklDQQZLBEAEYANmAx3Lxk-IIixSpp0pJOMwbKjpo6hWJ2CgT2mpozOtXKNrYNIH7NhJ3d-ZDUrL0DQ3GjCaATJXpWuToOlm7KLkamNYbLY7SR7TQHORHY6YVBheCJU4BVrBQaJWIjMaJV6HAjKOzSHZ6aoKfTifGrYpaTTWWx2SzScr2FzHJEtC53SAPTHPJLFHTaAiOVz4hTKFRyPaUgUEUkC8S5Fw6A7iMWspoBbnxcZiaSq9KZGxSRZ5OyUkpyWRfXIpFQ2aQs7yeIA */
     createMachine({
         id: "counter",
