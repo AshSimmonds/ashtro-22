8f83dbcd4e58c27830fe3ce458a8574bb73bfcbe
Ash Simmonds <ashsimmonds@gmail.com>
2022-10-21 13:15:48 +1030

    scaffold out burger consumer

---
+++ src/components/XStateBurger.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { burgerMachineService } from "../machines/burger"
+
+    let burgerState
+
+    burgerMachineService.onTransition((newState) => {
+        burgerState = newState
+    })
+</script>
+
+<div>
+    <div>asdf</div>
+
+    <pre>
+        {JSON.stringify(burgerState, null, 4)}
+    </pre>
+
+    <div>qwer</div>
+</div>
