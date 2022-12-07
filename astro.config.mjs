a9e5897fad70f2a9eca6a59ddd217f2618e16a5e
Ash Simmonds <ashsimmonds@gmail.com>
2022-12-08 04:09:33 +1030

    npx astro add solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import alpinejs from "@astrojs/alpinejs";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
+import solidJs from "@astrojs/solid-js";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs(), preact(), tailwind(), svelte(), vue()],
+  integrations: [alpinejs(), preact(), tailwind(), svelte(), vue(), solidJs()],
   output: 'server',
   adapter: vercel()
 });
