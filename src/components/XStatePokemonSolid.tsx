0955a4c145d0a725952c43404e0b4fd82e69fe4a
Ash Simmonds <ashsimmonds@gmail.com>
2022-12-08 19:39:11 +1030

    enable autofetching

--- src/components/XStatePokemonSolid.tsx
+++ src/components/XStatePokemonSolid.tsx
@@ -2,110 +2,191 @@ import { createSignal, Show } from "solid-js"
 import { createMachine, interpret } from 'xstate'
 // import { ErrorBoundary } from "solid-js"
 
-const pokemonMachine = createMachine({
-    id: 'pokemon',
-    initial: 'idle',
-    predictableActionArguments: true,
-    context: {
-        data: {} as any,
-        error: {} as any,
-        updates: 0,
-        updated: new Date(),
-        idleCount: 0,
-        fetchCount: 0,
-        state: 'dunno',
-    },
-    states: {
-        idle: {
-            entry: 'idleEntry',
-            on: {
-                fetch: {
-                    target: 'fetching',
+const artificialDelay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+const artificialDelayTime = 1000
+
+const pokemonMachine =
+    /** @xstate-layout N4IgpgJg5mDOIC5QAcD2BrMBbVA7AdKgGZEDEYuAhgEYA2YA2gAwC6iKqsAlgC5d7sQAD0QA2UfgAsAJgCMAVgDMTSQHZlkyQA5ZAGhABPRAFoF+OaNXTRyptK2jZWrQF8X+tJhwEuEeqSIwHgBjAAtmNiQQNG4+ASiRBCt8O3k5J3lZRXlJRVF9IwRZZIBOLXkS8S01VS11Nw8MbDx8QJDQrlwoUgg8MHxOgDcm1qCwgBFKHkoIwRjeflxBRMVVKQVq6RLJUTL5eS0CxElZaRTHbZPKhQrFBuim71H2zu6wACd31Hf8ZFopojfLDPCZTGasOacBbxUCJexMFI6JiqU72SpMPSGRDwqSqJglRSKaz7eT41T3TzNAhEACuwUwEHIVDojAhUXmcSWCWOWkU+B0jlkTEUakkTEOWIQJwR+1E0lUohyVkc8jc7hAuFQEDgkK8MIesUWyxMogRqhKGOk2hKslk2wORwQphRKRK0gqVkkBPldQpjxaxCIkMN+sSpvM9iJeNJJRKpOkjuMvPwpsUccq1hsmmkfr1Pj8YGD0K5sOxJSkWnd5UJ8lE1VOjvkazKGOU0nbKhOVtzVJBHS6Rc5xqdp3w5st1tt9olhStZ0kFQqTFTzlU2h7T1p9Mgg6N3IQ1lkUmkhIJTbqB0kjsU5X5KmqdVqJ7KapcQA */
+    createMachine({
+        id: 'pokemon',
+        initial: 'off',
+        predictableActionArguments: true,
+        context: {
+            data: {} as any,
+            error: {} as any,
+            updates: 0,
+            updated: new Date(),
+            idleCount: 0,
+            fetchCount: 0,
+            state: 'off',
+        },
+        states: {
+            off: {
+                on: {
+                    enable: 'idle',
                 },
             },
-        },
-        fetching: {
-            entry: 'fetchingEntry',
-            invoke: {
-                id: 'fetchData',
-                src: 'fetchData',
-                onDone: {
-                    target: 'idle',
+            idle: {
+                entry: 'idleEntry',
+                on: {
+                    fetch: {
+                        target: 'fetching',
+                    },
                 },
-                onError: {
-                    target: 'fucked',
+                after: {
+                    3333: {
+                        target: 'autoFetching',
+                    },
+                },
+            },
+            autoFetching: {
+                entry: 'autoFetchEntry',
+                after: {
+                    333: {
+                        target: 'fetching',
+                    },
+                },
+            },                    
+            fetching: {
+                entry: 'fetchingEntry',
+
+                invoke: {
+                    id: 'fetchData',
+                    src: 'fetchData',
+                    onDone: {
+                        target: 'idle',
+                    },
+                    onError: {
+                        target: 'fucked',
+                    },
+                },
+            },
+            fucked: {
+                entry: 'fuckedEntry',
+                type: 'final',
+                on: {
+                    enable: 'idle',
                 },
             },
 
         },
-        fucked: {
-            entry: 'fuckedEntry',
-            type: 'final',
-        },
+    }, {
+        actions: {
+            idleEntry: (context, event) => {
+                console.log(`idleEntry: ${event.type}`)
 
-    },
-}, {
-    actions: {
-        idleEntry: (context, event) => {
-            context.idleCount++
-            context.state = 'idle'
-            console.log(`idleEntry: ${event.type}`)
+                artificialDelay(artificialDelayTime).then(() => {
+                    context.idleCount++
+                    context.state = 'idle'
+                    console.log(`idleEntry waited artificialDelayTime: ${artificialDelayTime}ms`)
+                })
+            },
+            autoFetchEntry: (context, event) => {
+                context.state = 'autoFetching'
+                console.log(`autoFetchEntry: ${event.type}`)
+            },
+            fetchingEntry: (context, event) => {
+                context.fetchCount++
+                context.state = 'fetching'
+                console.log(`fetchingEntry: ${event.type}`)
+            },
+            fuckedEntry: (context, event) => {
+                context.state = 'fucked'
+                console.log(`fuckedEntry: ${JSON.stringify(event, null, 4)}`)
+            },
         },
-        fetchingEntry: (context, event) => {
-            context.fetchCount++
-            context.state = 'fetching'
-            console.log(`fetchingEntry: ${event.type}`)
-        },
-        fuckedEntry: (context, event) => {
-            context.state = 'fucked'
-            console.log(`fuckedEntry: ${JSON.stringify(event, null, 4)}`)
-        },
-    },
-    services: {
-        fetchData: (context, event) => {
-            console.log(`fetchData: ${event.type}`)
-            const randomNumber = Math.round(Math.random() * 260) + 1
-            const theUrl = `https://pokeapi.co/api/v2/ability/${randomNumber}`
+        services: {
+            fetchData: (context, event) => {
+                console.log(`fetchData: ${event.type}`)
+                const randomNumber = Math.round(Math.random() * 260) + 1
+                const theUrl = `https://pokeapi.co/api/v2/ability/${randomNumber}`
 
-            try {
-                return fetch(theUrl)
-                    .then((response) => response.json())
-                    .then((data) => {
-                        context.data = data
-                        context.updated = new Date()
-                        context.updates++
-                        return data
-                    })
-            } catch (error) {
-                context.error = error
-                console.error(`Error: ${error}`)
-                return error as any
-            }
+                try {
+                    return fetch(theUrl)
+                        .then((response) => response.json())
+                        .then((data) => {
+                            context.data = data
+                            context.updated = new Date()
+                            context.updates++
+                            return data
+                        })
+                } catch (error) {
+                    context.error = error
+                    console.error(`Error: ${error}`)
+                    return error as any
+                }
+            },
         },
-    },
-})
+    })
+
+
+
 
 
 const pokemonMachineService = interpret(pokemonMachine).start()
 
-
-
 const [pokemonState, setPokemonState] = createSignal(pokemonMachineService.getSnapshot())
 
 pokemonMachineService.onTransition((newState) => {
-    setPokemonState(newState)
+    console.log(`onTransition newState.value: ${newState.value}`)
+
+    if (newState.value !== 'asdf') {
+        setPokemonState(newState)
+    } else {
+        artificialDelay(artificialDelayTime).then(() => {
+            setPokemonState(newState)
+            console.log(`onTransition waited artificialDelayTime: ${artificialDelayTime}ms`)
+        })
+    }
 })
+
+pokemonMachineService.onChange((theContext) => {
+    console.log(`onChange theContext.state: ${theContext.state}`)
+})
+
+
+pokemonMachineService.onEvent((theEvent) => {
+    console.log(`onEvent theEvent: ${theEvent.type}`)
+})
+
+
+pokemonMachineService.onSend((theSend) => {
+    console.log(`onSend theSend: ${theSend}`)
+})
+
+
+pokemonMachineService.onStop(() => {
+    console.log(`onStop no context`)
+})
+
+
+pokemonMachineService.onDone((theDone) => {
+    console.log(`onDone theDone: ${JSON.stringify(theDone, null, 4)}`)
+})
+
 
 export default function PokemonSolid() {
 
     return (
 
         <div>
-            <button onclick={() => pokemonMachineService.send("fetch")} class="btn btn-success">Fetch</button>
+            <Show when={pokemonState().value === 'off'}>
+                <button onclick={() => pokemonMachineService.send("enable")} class="btn btn-error">Start</button>
+            </Show>
+            <Show when={pokemonState().value === 'idle'}>
+                <button onclick={() => pokemonMachineService.send("fetch")} class="btn btn-success">Fetch</button>
+            </Show>
+            <Show when={pokemonState().value === 'fetching' || pokemonState().value === 'autoFetching'}>
+                <button onclick={() => pokemonMachineService.send("cancel")} class="btn btn-warning">cancel</button>
+            </Show>
+            <Show when={pokemonState().value === 'fucked'}>
+                <button onclick={() => pokemonMachineService.send("enable")} class="btn btn-success">Fix</button>
+            </Show>
 
             <ul>
                 <li>
-                    State: <span class="badge text-xl">{pokemonState().context.state}</span>
+                    State: <span class="badge text-xl">{pokemonState().value.toString()}</span>
                 </li>
 
                 <li>
